6c6a6f69cd62621a38120c0cf238d78b1ab685cc
mertynel <mertynel65@gmail.com>
2017-12-30 21:35:24 +0200

    added btn eventlisteners

--- script.js
+++ script.js
@@ -5,7 +5,8 @@ let UIcontroller = function(){
         nvaLink_Game2: '.nav_link_game_2',
         game_1: 'Game_1.html',
         game_2: 'Game_2.html',
-        
+        btnStop: '.btn-stop',
+        btnStart: '.btn-start'
     }
     
     return {
@@ -18,15 +19,36 @@ let controller = function(UIctrl){
     let DOM = UIctrl.getDomStrings()
     
     let setupEventListeners = () => {
-        document.querySelector(DOM.nvaLink_Game1).addEventListener('click', () => {
-            $(DOM.content).load(DOM.game_1);
+            document.querySelector(DOM.nvaLink_Game1).addEventListener('click', () => {
+                $(DOM.content).load(
+                    DOM.game_1,
+                    function(){ 
+                        setupGame1()  
+                    })
+            })
+            document.querySelector(DOM.nvaLink_Game2).addEventListener('click', () => {
+                $(DOM.content).load(
+                    DOM.game_2,
+                    function(){
+                        setupGame2()
+                    })
+            })
+    }
+    
+	let setupGame1 = function()
+    {
+        document.querySelector(DOM.btnStart).addEventListener('click', () => {
+            console.log('game start')
         })
-        document.querySelector(DOM.nvaLink_Game2).addEventListener('click', () => {
-            $(DOM.content).load(DOM.game_2);
+        document.querySelector(DOM.btnStop).addEventListener('click', () => {
+            console.log('game stop')
         })
     }
     
-    
+    let setupGame2 = function()
+    {
+
+    }
     
     return {
         init: function(){
